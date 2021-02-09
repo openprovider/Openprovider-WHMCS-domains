98128720eca01114ca1c68c7de82bc1445891b5d
D <sibgat.d.s@gmail.com>
2021-02-09 10:53:20 +0700

    [BUGFIX] MODULES-44 - customer getting oops error with mixed registrars

--- includes/additionalfields.php
+++ includes/additionalfields.php
@@ -4,4 +4,5 @@
 * Put this code above the other fields. Don't override additional fields manually for Openprovider: we maintain this for you.
 */
 
-$additionaldomainfields = openprovider_additional_fields();
+if (function_exists('openprovider_additional_fields'))
+    $additionaldomainfields = openprovider_additional_fields();
