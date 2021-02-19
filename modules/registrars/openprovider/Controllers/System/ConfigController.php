964dbf9f4e408dc198d4abb274fc973fed0a2c2b
D <sibgat.d.s@gmail.com>
2021-02-19 17:58:09 +0700

    [HOTFIX] #MODULES-48 updated warning message

--- modules/registrars/openprovider/Controllers/System/ConfigController.php
+++ modules/registrars/openprovider/Controllers/System/ConfigController.php
@@ -15,7 +15,7 @@ class ConfigController extends BaseController
     const ERROR_INCORRECT_CREDENTIALS = 'Please ensure credentials are correct';
     const ERROR_INCORRECT_INVIRONMENT = 'Please ensure environment is correct';
     const ERROR_NOT_SIGNED_AGREEMENT  = 'Account is blocked because of non-signed agreement. Log in to the control panel for more information.';
-    const ERROR_NOT_HAVE_AUTHORITY    = 'This ip address does not have authority to make API calls with this account. Please check the white and black lables of your account.';
+    const ERROR_NOT_HAVE_AUTHORITY    = 'This ip address does not have authority to make API calls with this account. Please check the IP whitelist and blacklist of your account.';
 
     /**
      * @var API
