e7f44506c30c07adb4b717f7146b048fc59e227a
D <sibgat.d.s@gmail.com>
2021-02-20 18:39:10 +0700

    update

--- modules/registrars/openprovider/scripts/UpdateGuzzleNamespace.php
+++ modules/registrars/openprovider/scripts/UpdateGuzzleNamespace.php
@@ -9,7 +9,6 @@ class UpdateGuzzleNamespace
     public static function postUpdate(Event $event)
     {
         $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
-
         $vendorStaticDir = "{$vendorDir}/../vendor-static";
         
         if (!is_dir("{$vendorDir}/guzzlehttp")) {
@@ -17,19 +16,22 @@ class UpdateGuzzleNamespace
         }
         
         self::replaceGuzzleInDirectory("{$vendorDir}/guzzlehttp");
-        self::replaceGuzzleInDirectory("{$vendorDir}/openprovider");
-
+        self::replaceGuzzleInDirectory("{$vendorDir}/openprovider/rest-client-php/src");
         self::removeDir("{$vendorStaticDir}/guzzlehttp");
         self::moveDir("{$vendorDir}/guzzlehttp", $vendorStaticDir);
     }
-
+    
     private static function replaceGuzzleInDirectory(string $pathDir)
     {
+        if (!is_dir($pathDir)) {
+            return;
+        }
+        
         $directory = opendir($pathDir);
-
-        while ($element = readdir($directory))
-        {
-            if ($element == '.' || $element == '..'){
+        
+        while ($element = readdir($directory)) {
+        
+            if ($element == '.' || $element == '..') {
                 continue;
             }
             
@@ -37,19 +39,19 @@ class UpdateGuzzleNamespace
                 self::replaceGuzzleInDirectory("{$pathDir}/{$element}");
                 continue;
             }
-
+            
             $filePath = "{$pathDir}/{$element}";
             $fileContent = file_get_contents($filePath);
-            $replacedFileContent = preg_replace('/GuzzleHttp6*/', 'GuzzleHttp6', $fileContent);
-            file_put_contents($filePath, $replacedFileContent);
+            $fileContent = preg_replace('/GuzzleHttp6*/', 'GuzzleHttp6', $fileContent);
+            file_put_contents($filePath, $fileContent);
         }
     }
-
+    
     private static function moveDir(string $pathFrom, string $pathTo)
     {
         shell_exec("mkdir --parents {$pathTo}; mv {$pathFrom} {$pathTo}");
     }
-
+    
     private static function removeDir(string $path)
     {
         shell_exec("rm -rf {$path}");
