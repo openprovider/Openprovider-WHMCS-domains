c387f6904cf7cf85c540083728398aa80f4d032e
sapillai <97894083+sapillai@users.noreply.github.com>
2023-02-28 15:21:14 +0530

    Update openprovider.php
    Fix for https://github.com/openprovider/Openprovider-WHMCS-domains/issues/256

--- modules/registrars/openprovider/openprovider.php
+++ modules/registrars/openprovider/openprovider.php
@@ -341,7 +341,11 @@ function openprovider_ResendIRTPVerificationEmail(array $params)
 function openprovider_registrar_launch_decorator(string $route, $params = [], $level = 'system')
 {
     $modifiedParams = array_merge($params, Configuration::getParams());
+    if (isset($params['original'])) {
     $modifiedParams['original'] = array_merge($params['original'], Configuration::getParams());
+} else {
+    $modifiedParams['original'] = Configuration::getParams();
+       }
 
     $core = openprovider_registrar_core($level);
     $launch = $core->launch();
