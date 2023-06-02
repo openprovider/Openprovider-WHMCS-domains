f74edc53dca75139c065bf175918856d1d1d6cd6
sapillai <97894083+sapillai@users.noreply.github.com>
2023-06-02 15:09:09 +0530

    Update APIConfig.php
    Version change

--- modules/registrars/openprovider/OpenProvider/API/APIConfig.php
+++ modules/registrars/openprovider/OpenProvider/API/APIConfig.php
@@ -11,7 +11,7 @@ namespace OpenProvider\API;
 
 class APIConfig
 {
-    static public $moduleVersion     = 'whmcs-5.6-REST';
+    static public $moduleVersion     = 'whmcs-5.7.0-REST';
     static public $supportedDnsTypes = array('A', 'AAAA', 'CAA', 'CNAME', 'MX', 'SPF', 'SSHFP', 'SRV', 'TLSA', 'TXT');
     static public $dnsRecordTtl      = 86400;
     static public $dnsRecordPriority = 10;
