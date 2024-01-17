da8dc294b8335cfebd4fb452c079ce87e74b7ec0
Uditha-05 <uditha.isuranga@openprovider.nl>
2024-01-17 17:17:17 +0530

    Moved dnssec.php file into "registrars/openprovider/custom-pages" directory from addon directory

--- modules/registrars/openprovider/Controllers/Hooks/ClientAreaPrimarySidebarController.php
+++ modules/registrars/openprovider/Controllers/Hooks/ClientAreaPrimarySidebarController.php
@@ -67,11 +67,11 @@ jQuery( document ).ready(function() {
             !file_exists($GLOBALS['whmcsAppConfig']->getRootDir() . self::DNSSEC_PAGE_NAME) ||
             empty(file_get_contents($GLOBALS['whmcsAppConfig']->getRootDir() . self::DNSSEC_PAGE_NAME))
         ){
-            $source_location = $GLOBALS['whmcsAppConfig']->getRootDir()."/modules/addons/openprovider/custom-pages" . self::DNSSEC_PAGE_NAME;
+            $source_location = $GLOBALS['whmcsAppConfig']->getRootDir()."/modules/registrars/openprovider/custom-pages" . self::DNSSEC_PAGE_NAME;
             $destination_location = $GLOBALS['whmcsAppConfig']->getRootDir() . self::DNSSEC_PAGE_NAME;
             // Attempt to copy dnssec.php file into the root file
             if (!copy($source_location, $destination_location)) {
-                logModuleCall('openprovider', 'copydnssecfile', null, "DNSSEC page error! Fail to add dnssec.php to root directory. Please manually upload the contents of '<Module directory>/addons/openprovider/custom-pages' to the top level of your WHMCS folder i.e. '<your WHMCS directory>/'" , null, null);
+                logModuleCall('openprovider', 'copydnssecfile', null, "DNSSEC page error! Fail to add dnssec.php to root directory. Please manually upload the contents of '<Module directory>/registrars/openprovider/custom-pages' to the top level of your WHMCS folder i.e. '<your WHMCS directory>/'" , null, null);
             }
         }
         
