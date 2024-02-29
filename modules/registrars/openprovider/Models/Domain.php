c580127f9eff62d3d80d40792845f207bb5ab5b1
Uditha-05 <uditha.isuranga@openprovider.nl>
2024-02-29 12:12:52 +0530

    Fixed PHP version incompatible issue

--- modules/registrars/openprovider/Models/Domain.php
+++ modules/registrars/openprovider/Models/Domain.php
@@ -22,9 +22,9 @@ class Domain extends \WeDevelopCoffee\wPower\Models\Domain
         $setting_value = Configuration::getOrDefault('renewTldsUponTransferCompletion', '');
 
         // When nothing was found; return false.
-        if(count($setting_value) == 0
-            || count($setting_value ) && $setting_value == '')
+        if(!is_string($setting_value) || $setting_value == ''){
             return false;
+        }
 
         $tlds = explode(",",$setting_value);
 
