d22b1b662a837d219d03688ec867627c36dfdb8d
Uditha-05 <uditha.isuranga@openprovider.nl>
2024-07-12 17:47:15 +0530

    Info message added to 'TLD Import & Pricing Sync' page

---
+++ modules/registrars/openprovider/Controllers/Hooks/AdminAreaHeaderOutputController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace OpenProvider\WhmcsRegistrar\Controllers\Hooks;
+
+use WHMCS\Database\Capsule;
+
+class AdminAreaHeaderOutputController
+{
+    public function output($vars)
+    {
+        $isTestMode = false;
+
+        $rows = Capsule::table('tblregistrars')->get();
+
+        foreach ($rows as $row) {
+            $registrar = $row->registrar;
+            $setting = $row->setting;
+            $value = decrypt($row->value); // Decrypt the value if needed
+
+            if ($registrar == 'openprovider' && $setting == 'test_mode') {
+                if ($value == 'on') {
+                    $isTestMode = true;
+                } else {
+                    $isTestMode = false;
+                }
+                break;
+            }
+        }
+
+        $return = '';
+        if ($vars["pagetitle"] == 'TLD Import & Pricing Sync' && $isTestMode) {
+            $return = '<div class="alert alert-danger global-admin-warning"> <i class="far fa-exclamation-triangle fa-fw"></i> <b>Prices in the Sandbox environment are for reference only and may differ from actual domain prices in the production environment.</b></div>';
+            return $return;
+        }
+
+        return $return;
+    }
+}
