70cc5467a18b578efa220aba6ab3af0b1aec07c4
fioniait-dennis <dennis@chosting.dk>
2025-01-20 20:59:23 +0100

    Automatically display additional fields in cart

--- modules/registrars/openprovider/openprovider.php
+++ modules/registrars/openprovider/openprovider.php
@@ -393,3 +393,16 @@ function openprovider_registrar_launch_decorator(string $route, $params = [], $l
 
     return $launch->output($modifiedParams, $route);
 }
+
+function openprovider_AdditionalDomainFields(array $params)
+{
+    $additionalfields = openprovider_additional_fields();
+
+    if (isset($additionalfields[".{$params['tld']}"])) {
+        $values["fields"] = $additionalfields[".{$params['tld']}"];
+    } else {
+        $values["fields"] = [];
+    }
+
+    return $values;
+}
