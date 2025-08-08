f3279efe67dd9c6f84ff23381545bba613ec9b60
Daniel Hofbauer | RATH-iT <dho@rath-it.at>
2025-08-08 12:17:19 +0200

    Added german language support

---
+++ lang/overrides/german.php
@@ -0,0 +1,36 @@
+<?php
+
+$_LANG['dnssectabname'] = 'DNSSEC Verwaltung';
+
+$_LANG['dnssec']['pagename']  = 'DNSSEC Einträge';
+$_LANG['dnssec']['tablename'] = 'DNSSEC Einträge verwalten';
+
+$_LANG['dnssec']['activatednssecbutton']   = 'DNSSEC aktivieren';
+$_LANG['dnssec']['deactivatednssecbutton'] = 'DNSSEC deaktivieren';
+
+
+$_LANG['dnssec']['alertdnssecnotactivated'] = 'DNSSEC ist nicht aktiviert auf dieser Domain.';
+$_LANG['dnssec']['alertdnssecactivated']     = 'DNSSEC ist auf dieser Domain aktiv. Wenn DNSSEC deaktiviert wird, werden alle DNSSEC-Einträge gelöscht.';
+
+$_LANG['dnssec']['tableheaderflags']       = 'Flag';
+$_LANG['dnssec']['tableheaderalgorithms']  = 'Algorithmus';
+$_LANG['dnssec']['tableheaderpublickeys'] = 'Public key';
+$_LANG['dnssec']['tableheaderactions']     = 'Aktionen';
+
+
+$_LANG['dnssec']['tablebuttonactiondelete'] = 'Löschen';
+$_LANG['dnssec']['tablebuttonactionsave']   = 'Speichern';
+
+$_LANG['dnssec']['buttonadddnssecrecord'] = 'Einen neuen DNSSEC-Eintrag hinzufügen';
+
+$_LANG['esIdentificationType'] = 'Identification Type';
+$_LANG['esIdentificationNumber'] = "Company or Individual ID";
+$_LANG['esIdentificationPassport'] = "Passport/Individual ID";
+$_LANG['esIdentificationCompany'] = "Company Registration ID";
+$_LANG['esIdentificationCORI'] = "Company or Individual ID";
+
+$_LANG['ptIdentificationType'] = 'Tipo de Contribuinte (VAT/TAX ID)';
+$_LANG['ptIdentificationNumber'] = 'Tipo de Contribuinte (VAT/TAX ID)';
+$_LANG['ptIdentificationVat'] = "NIPC (empresa)";
+$_LANG['ptIdentificationSocialSecurityNumber'] = "NIF (particular)";
+$_LANG['ptIdentificationCORI'] = 'Tipo de Contribuinte (VAT/TAX ID)';
