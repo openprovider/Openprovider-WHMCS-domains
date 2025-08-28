0205adbaaf69363748c1e67d7546cc25d5ea196e
Daniel Hofbauer | RATH-iT <dho@rath-it.at>
2025-08-28 16:21:05 +0200

    Adjustments to german translation
    Adjusted to improvments made by @avishka-ra.

--- lang/overrides/german.php
+++ lang/overrides/german.php
@@ -9,19 +9,19 @@ $_LANG['dnssec']['activatednssecbutton']   = 'DNSSEC aktivieren';
 $_LANG['dnssec']['deactivatednssecbutton'] = 'DNSSEC deaktivieren';
 
 
-$_LANG['dnssec']['alertdnssecnotactivated'] = 'DNSSEC ist nicht aktiviert auf dieser Domain.';
-$_LANG['dnssec']['alertdnssecactivated']     = 'DNSSEC ist auf dieser Domain aktiv. Wenn DNSSEC deaktiviert wird, werden alle DNSSEC-Einträge gelöscht.';
+$_LANG['dnssec']['alertdnssecnotactivated'] = 'DNSSEC ist für diese Domain nicht aktiv.';
+$_LANG['dnssec']['alertdnssecactivated']     = 'DNSSEC ist auf dieser Domain aktiv. Wenn Sie DNSSEC deaktivieren, werden alle vorhandenen Schlüssel aus dieser Domain gelöscht.';
 
-$_LANG['dnssec']['tableheaderflags']       = 'Flag';
+$_LANG['dnssec']['tableheaderflags']       = 'Flags';
 $_LANG['dnssec']['tableheaderalgorithms']  = 'Algorithmus';
-$_LANG['dnssec']['tableheaderpublickeys'] = 'Public key';
+$_LANG['dnssec']['tableheaderpublickeys'] = 'Öffentlicher Schlüssel';
 $_LANG['dnssec']['tableheaderactions']     = 'Aktionen';
 
 
 $_LANG['dnssec']['tablebuttonactiondelete'] = 'Löschen';
 $_LANG['dnssec']['tablebuttonactionsave']   = 'Speichern';
 
-$_LANG['dnssec']['buttonadddnssecrecord'] = 'Einen neuen DNSSEC-Eintrag hinzufügen';
+$_LANG['dnssec']['buttonadddnssecrecord'] = 'Neuen DNSSEC-Eintrag hinzufügen';
 
 $_LANG['esIdentificationType'] = 'Identification Type';
 $_LANG['esIdentificationNumber'] = "Company or Individual ID";
