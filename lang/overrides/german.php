621ad8f0b2d7a8766a47391dbbc0563cf16e37b9
avishka-ra <avishka.rathnavibhushana@openprovider.nl>
2026-01-16 16:07:06 +1030

    Remove inconsistance spacing in german.php

--- lang/overrides/german.php
+++ lang/overrides/german.php
@@ -2,25 +2,25 @@
 
 $_LANG['dnssectabname'] = 'DNSSEC Verwaltung';
 
-$_LANG['dnssec']['pagename']  = 'DNSSEC Einträge';
+$_LANG['dnssec']['pagename'] = 'DNSSEC Einträge';
 $_LANG['dnssec']['tablename'] = 'DNSSEC Einträge verwalten';
 
-$_LANG['dnssec']['activatednssecbutton']   = 'DNSSEC aktivieren';
+$_LANG['dnssec']['activatednssecbutton'] = 'DNSSEC aktivieren';
 $_LANG['dnssec']['deactivatednssecbutton'] = 'DNSSEC deaktivieren';
 
 
 $_LANG['dnssec']['alertdnssecnotactivated'] = 'DNSSEC ist für diese Domain nicht aktiv.';
-$_LANG['dnssec']['alertdnssecactivated']     = 'DNSSEC ist auf dieser Domain aktiv. Wenn Sie DNSSEC deaktivieren, werden alle vorhandenen Schlüssel aus dieser Domain gelöscht.';
+$_LANG['dnssec']['alertdnssecactivated'] = 'DNSSEC ist auf dieser Domain aktiv. Wenn Sie DNSSEC deaktivieren, werden alle vorhandenen Schlüssel aus dieser Domain gelöscht.';
 
 
-$_LANG['dnssec']['tableheaderflags']       = 'Flags';
-$_LANG['dnssec']['tableheaderalgorithms']  = 'Algorithmus';
+$_LANG['dnssec']['tableheaderflags'] = 'Flags';
+$_LANG['dnssec']['tableheaderalgorithms'] = 'Algorithmus';
 $_LANG['dnssec']['tableheaderpublickeys'] = 'Öffentlicher Schlüssel';
-$_LANG['dnssec']['tableheaderactions']     = 'Aktionen';
+$_LANG['dnssec']['tableheaderactions'] = 'Aktionen';
 
 
 $_LANG['dnssec']['tablebuttonactiondelete'] = 'Löschen';
-$_LANG['dnssec']['tablebuttonactionsave']   = 'Speichern';
+$_LANG['dnssec']['tablebuttonactionsave'] = 'Speichern';
 
 $_LANG['dnssec']['buttonadddnssecrecord'] = 'Neuen DNSSEC-Eintrag hinzufügen';
 
