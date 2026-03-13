c7fb8422d6021143a6ec1cd79995454a1f4f6426
Dennis Hermannsen <dennis@chosting.dk>
2026-03-13 11:35:13 +0100

    (chore) Change variable name and add openprovider key

---
+++ modules/registrars/openprovider/lang/english.php
@@ -0,0 +1,37 @@
+<?php
+
+$_ADDONLANG['openprovider']['dnssectabname'] = 'DNSSEC Management';
+
+$_ADDONLANG['openprovider']['dnssec']['pagename']  = 'DNSSEC Records';
+$_ADDONLANG['openprovider']['dnssec']['tablename'] = 'Manage DNSSEC Records';
+
+$_ADDONLANG['openprovider']['dnssec']['activatednssecbutton']   = 'Activate DNSSEC';
+$_ADDONLANG['openprovider']['dnssec']['deactivatednssecbutton'] = 'Deactivate DNSSEC';
+
+
+$_ADDONLANG['openprovider']['dnssec']['alertdnssecnotactivated'] = 'DNSSEC is not active on this domain.';
+$_ADDONLANG['openprovider']['dnssec']['alertdnssecactivated']     = 'DNSSEC is active for this domain. If you deactivate DNSSEC, all existing keys will be deleted from this domain.';
+
+
+$_ADDONLANG['openprovider']['dnssec']['tableheaderflags']       = 'Flags';
+$_ADDONLANG['openprovider']['dnssec']['tableheaderalgorithms']  = 'Algorithm';
+$_ADDONLANG['openprovider']['dnssec']['tableheaderpublickeys'] = 'Public key';
+$_ADDONLANG['openprovider']['dnssec']['tableheaderactions']     = 'Actions';
+
+
+$_ADDONLANG['openprovider']['dnssec']['tablebuttonactiondelete'] = 'Delete';
+$_ADDONLANG['openprovider']['dnssec']['tablebuttonactionsave']   = 'Save';
+
+$_ADDONLANG['openprovider']['dnssec']['buttonadddnssecrecord'] = 'Add A New DNSSEC Record';
+
+$_ADDONLANG['openprovider']['esIdentificationType'] = 'Identification Type';
+$_ADDONLANG['openprovider']['esIdentificationNumber'] = "Company or Individual ID";
+$_ADDONLANG['openprovider']['esIdentificationPassport'] = "Passport/Individual ID";
+$_ADDONLANG['openprovider']['esIdentificationCompany'] = "Company Registration ID";
+$_ADDONLANG['openprovider']['esIdentificationCORI'] = "Company or Individual ID";
+
+$_ADDONLANG['openprovider']['ptIdentificationType'] = 'Tipo de Contribuinte (VAT/TAX ID)';
+$_ADDONLANG['openprovider']['ptIdentificationNumber'] = 'Tipo de Contribuinte (VAT/TAX ID)';
+$_ADDONLANG['openprovider']['ptIdentificationVat'] = "NIPC (empresa)";
+$_ADDONLANG['openprovider']['ptIdentificationSocialSecurityNumber'] = "NIF (particular)";
+$_ADDONLANG['openprovider']['ptIdentificationCORI'] = 'Tipo de Contribuinte (VAT/TAX ID)';
